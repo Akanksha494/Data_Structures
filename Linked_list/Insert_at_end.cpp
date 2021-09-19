062f995e27012893baac8982fa00d23a3223d601
akanksha kasaudhan <82376735+Akanksha494@users.noreply.github.com>
2021-09-19 11:23:57 -0700

    insert at end

---
+++ Linked_list/Insert_at_end.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *next;
+};
+
+void insertatend(Node **head, int data) {
+    
+    Node *node = new Node();
+    
+    node -> data = data;
+    node -> next = NULL;
+    
+    if(*head == NULL) {
+        *head = node;
+        
+    } else {
+        Node *temp = *head;
+        
+        while(temp -> next != NULL) {
+            temp = temp -> next;
+        }
+        
+        temp -> next = node;
+    }
+}
+void printList(Node *head) {
+    Node *temp = head;
+    
+    while(temp != NULL) {
+        cout << temp -> data << " ";
+        temp = temp -> next;
+    }
+    
+    cout << endl;
+}
+
+int main()
+{
+    Node *head = NULL; 
+    
+    int a;
+    
+    for(int i = 0; i < 5; i++) {
+        cin >> a;
+        
+        insertatend(&head, a);
+    }
+    
+    printList(head);
+}
+    
